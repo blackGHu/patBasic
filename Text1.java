6e3ce5ca3e196809829659346538c869a9e5621d
blackGHu <1292754686@qq.com>
2017-12-30 17:40:14 +0800

    none

--- Text1.java
+++ Text1.java
@@ -1,26 +1,27 @@
 import java.util.*;
 
-public class Text1 {
-    public static void main(String []args) {
-        Scanner in = new Scanner(System.in);
-        boolean flag = false;
-        while (in.hasNextInt()) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            if (a * b != 0) {
-                if (!flag) {
-                    System.out.print(a * b + " " + (b - 1));
-                } else {
-                    flag = false;
-                    System.out.print(" ");
-                }
-
-            }
-
-        }
-        in.close();
-        if (!flag) {
-            System.out.print("0 0");
-        }
-    }
+public class Text1{
+	public static void  main(String []args) {
+		Scanner in = new Scanner(System.in);
+		boolean flag = false;
+		while(in.hasNextInt()){
+			int a = in.nextInt();
+			int b = in.nextInt();
+			if(a*b!=0){
+				if(flag){
+					System.out.print(" ");
+				}
+				else {
+					flag = true;
+					
+				}
+				System.out.print(a*b+" "+(b-1));
+			}
+			
+		}
+		in.close();
+		if(!flag){
+			System.out.print("0 0");
+		}
+	}
 }
